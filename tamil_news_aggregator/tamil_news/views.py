2d7e1e0307fcee4ddedb4a62fedaa8b05dc0c222
amirthasriomraj <amirthasriomraj@gmail.com>
2025-07-11 14:20:56 +0530

    Update html and views

--- tamil_news_aggregator/tamil_news/views.py
+++ tamil_news_aggregator/tamil_news/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.utils.dateparse import parse_date
-
 from rest_framework import viewsets, filters
-from .models import NewsDetails, Websites
+
+from .models import NewsDetails, Websites, SentimentResults
 from .serializers import WebsiteSerializer, NewsDetailsSerializer
 
 
@@ -27,21 +27,24 @@ class NewsDetailsViewSet(viewsets.ModelViewSet):
 
 # Frontend View
 def news_list(request):
-    news = NewsDetails.objects.all().order_by('-published_time', '-id')
+    news = NewsDetails.objects.select_related("sentiment").order_by('-published_time', '-id')
     websites = Websites.objects.all()
 
     website_id = request.GET.get('website')
     category = request.GET.get('category')
+    sentiment_label = request.GET.get('sentiment')
     search_query = request.GET.get('search')
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
 
-    # 🔍 Filters
     if website_id:
         news = news.filter(website_id=website_id)
 
     if category:
         news = news.filter(category__icontains=category)
+
+    if sentiment_label:
+        news = news.filter(sentiment__sentiment_label=sentiment_label)
 
     if search_query and search_query.lower() != 'none':
         news = news.filter(
@@ -57,19 +60,21 @@ def news_list(request):
             news = news.filter(published_time__isnull=False)
             news = news.filter(published_time__date__range=(start, end))
 
-    # Pagination
     paginator = Paginator(news, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     categories = NewsDetails.objects.values_list('category', flat=True).distinct()
+    sentiments = SentimentResults.objects.values_list('sentiment_label', flat=True).distinct()
 
     context = {
         'news_list': page_obj,
         'websites': websites,
         'categories': categories,
+        'sentiments': sentiments,
         'website_id': website_id,
         'category': category,
+        'sentiment_label': sentiment_label,
         'search_query': search_query,
         'start_date': start_date,
         'end_date': end_date,
