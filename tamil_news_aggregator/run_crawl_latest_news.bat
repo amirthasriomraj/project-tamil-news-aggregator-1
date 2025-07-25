7002548e5f0148226e2a132b4930d702d823fb26
amirthasriomraj <amirthasriomraj@gmail.com>
2025-07-25 19:54:53 +0530

    Delete older scripts

--- tamil_news_aggregator/run_crawl_latest_news.bat
+++
@@ -1,3 +0,0 @@
-cd /d D:\Amirtha\project-tamil-news-aggregator\project-tamil-news-aggregator-1\tamil_news_aggregator
-call ..\..\myenv\Scripts\activate.bat
-python manage.py crawl_latest_news_logging
